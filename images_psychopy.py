bb57a66a6e492b70159efdce3e02a4a87345bfc3
corentin <corentin.warzee@student.uclouvain.be>
2024-08-07 18:21:51 +0200

    version avec signal

--- images_psychopy.py
+++ images_psychopy.py
@@ -1,56 +1,30 @@
 import argparse
 from psychopy import visual, core, event
+import serial
 
-def affichage_mots(win, text_stim, words, display_time):
-    for word in words:
-        text_stim.setText(word)
-        text_stim.draw()
-        win.flip()
-        core.wait(display_time)
+
+def wait_for_trigger(port='COM3', baudrate=9600, trigger_char='s'):
+    with serial.Serial(port, baudrate=baudrate) as ser:
+        trigger = ser.read().decode('utf-8')
+        while trigger != trigger_char:
+            trigger = ser.read().decode('utf-8')
+        print("Trigger received")
+
 
 def reading(filename):
-    filename= filename
     with open(filename, "r") as fichier:
         ma_liste = [line.strip() for line in fichier]
-    print(ma_liste)
     return ma_liste
 
-def words_psychopy(words, display_time, boolean):
-    print(words)
-    """
-    win = visual.Window(fullscr=True, color=[-1, -1, -1], units='pix')
-    if boolean:
-        text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=150)
-    else:
-        text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=50)
-    text_stim.setText("")
-    text_stim.draw()
-    win.flip()
-    event.waitKeys()
-    nothinkinglist = [":; $+", " #^=-", ":?$µ", "###"]
-    affichage_mots(win, text_stim, nothinkinglist, display_time)
-    affichage_mots(win, text_stim, words, display_time)
-    affichage_mots(win, text_stim, nothinkinglist, display_time)
-    event.waitKeys()
-    win.close()
-    core.quit()
-    """
 
-def static_images_psychopy(chemin,duration, betweenstimuli, zoom):
-    chemin = "Paradigme_images_statiques/"+chemin
-    images = reading(chemin)
-    if zoom:
-        thezoom=1
-    else:
-        thezoom=0.5
-
+def static_images_psychopy(chemin, duration, betweenstimuli, zoom):
     win = visual.Window(
         fullscr=True,
         color=[-0.0118, 0.0039, -0.0196],
         units="pix"
     )
-
-    # Créer le stimulus de croix
+    chemin="Paradigme_images_statiques/"+chemin
+    images = reading(chemin)
     cross_stim = visual.ShapeStim(
         win=win,
         vertices=((0, -20), (0, 20), (0, 0), (-20, 0), (20, 0)),
@@ -58,46 +32,51 @@ def static_images_psychopy(chemin,duration, betweenstimuli, zoom):
         closeShape=False,
         lineColor="white"
     )
+    thezoom = 1 if zoom else 0.5
+    timer = core.Clock()  # Horloge pour le timing précis
 
     cross_stim.draw()
     win.flip()
-    event.waitKeys()
+    wait_for_trigger()
 
     for image in images:
-        chemin = "Paradigme_images_statiques/stim_static/" + image
-        print(chemin)
+        image_path = "Paradigme_images_statiques/stim_static/" + image
         image_stim = visual.ImageStim(
             win=win,
-            image=chemin,  # Remplacez par le chemin de votre image
-            pos=(0, 0)  # Position centrale
+            image=image_path,
+            pos=(0, 0),
+            size=None  # Utilisez None pour conserver la taille originale ou ajustez selon 'thezoom'
         )
         image_stim.size *= thezoom
-        # image_stim.ori=45
-        # Dessiner l'image
+
         image_stim.draw()
         win.flip()
-        core.wait(duration)
+        timer.reset()  # Réinitialiser le timer à chaque nouvelle image
+        while timer.getTime() < duration:
+            pass  # Attente active jusqu'à la fin de la durée
+
+        # Afficher la croix entre les images
         cross_stim.draw()
         win.flip()
-        core.wait(betweenstimuli)
+        timer.reset()
+        while timer.getTime() < betweenstimuli:
+            pass  # Attente active entre les images
+
     win.close()
 
+
 def main(duration, betweenstimuli, file, zoom):
-    print(f"Durée: {duration}")
-    if file !="":
-        if zoom =="Activé":
-            static_images_psychopy(file, duration, betweenstimuli, True)
-        else:
-            static_images_psychopy(file, duration, betweenstimuli, False)
+    #wait_for_trigger()
+    static_images_psychopy(file, duration, betweenstimuli, zoom)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Exécuter le paradigme Psychopy")
-    parser.add_argument("--duration", type=str, required=True, help="Durée en secondes des stimuli")
-    parser.add_argument("--betweenstimuli", type=str, required=True, help="Durée en secondes entre les stimuli")
-    parser.add_argument("--file",required=False, type=str, help="Liste de mots pour le paradigme")
-    parser.add_argument("--zoom", type=str, choices=['Activé', 'Désactivé'], required=True, help="Activer le Zoom")
+    parser.add_argument("--duration", type=int, required=True, help="Durée en secondes des stimuli")
+    parser.add_argument("--betweenstimuli", type=int, required=True, help="Durée en secondes entre les stimuli")
+    parser.add_argument("--file", type=str, help="Chemin du fichier contenant les stimuli")
+    parser.add_argument("--zoom", type=str, choices=['Activé', 'Désactivé'], required=True,
+                        help="Activer ou désactiver le Zoom")
     args = parser.parse_args()
-    print(args.zoom)
-    print("on arrive jamais ici")
-    main(int(args.duration), int(args.betweenstimuli), args.file,args.zoom)
+
+    main(args.duration, args.betweenstimuli, args.file, args.zoom == 'Activé')
--- psychopy_runner.py
+++ psychopy_runner.py
@@ -1,69 +1,74 @@
 import argparse
+import serial
 from psychopy import visual, core, event
 
+
+def wait_for_trigger(port='COM3', baudrate=9600, trigger_char='s'):
+    with serial.Serial(port, baudrate=baudrate) as ser:
+        trigger = ser.read().decode('utf-8')
+        while trigger != trigger_char:
+            trigger = ser.read().decode('utf-8')
+        print("Trigger received")
+
+
 def affichage_mots(win, text_stim, words, display_time):
+    timer = core.Clock()
     for word in words:
         text_stim.setText(word)
         text_stim.draw()
         win.flip()
-        core.wait(display_time)
+        timer.reset()  # Réinitialiser l'horloge à chaque nouveau mot
+        while timer.getTime() < display_time:
+            pass  # Attendre sans bloquer d'autres processus
+
 
 def reading(filename):
-    filename= "Paradigme_mots/" + filename
+    filename = "Paradigme_mots/" + filename
     with open(filename, "r") as fichier:
         ma_liste = [line.strip() for line in fichier]
     print(ma_liste)
     return ma_liste
 
-def words_psychopy(words, display_time, boolean):
-    print(words)
 
+def pause_for_seconds(seconds):
+    timer = core.Clock()
+    timer.reset()
+    while timer.getTime() < seconds:
+        pass
+
+
+def words_psychopy(words, display_time, boolean):
     win = visual.Window(fullscr=True, color=[-1, -1, -1], units='pix')
-    if boolean:
-        text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=150)
-    else:
-        text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=50)
-    text_stim.setText("")
-    text_stim.draw()
-    win.flip()
-    event.waitKeys()
+    text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=150 if boolean else 50)
+
+    wait_for_trigger()
     nothinkinglist = [":; $+", " #^=-", ":?$µ", "###"]
     affichage_mots(win, text_stim, nothinkinglist, display_time)
     affichage_mots(win, text_stim, words, display_time)
     affichage_mots(win, text_stim, nothinkinglist, display_time)
-    event.waitKeys()
+    pause_for_seconds(5)  # Pause de 5 secondes avant de fermer
+
     win.close()
     core.quit()
 
 
 def main(duration, words, zoom, file):
     print(f"Durée: {duration}")
-    if file !="":
-        words=reading(file)
-        if (zoom == "Activé"):
-            words_psychopy(words, int(duration), True)
-        else:
-            words_psychopy(words, int(duration), False)
+    if file:
+        words = reading(file)
     else:
-        split_list = words.split(',')
-        cleaned_list = [item.strip() for item in split_list]
-        if (zoom=="Activé"):
-            words_psychopy(cleaned_list,int(duration),True)
-        else:
-            words_psychopy(cleaned_list,int(duration),False)
+        words = [word.strip() for word in words.split(',')]
+
+    #wait_for_trigger()
+    words_psychopy(words, int(duration), zoom == "Activé")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Exécuter le paradigme Psychopy")
     parser.add_argument("--duration", type=str, required=True, help="Durée en secondes des stimuli")
     parser.add_argument("--words", type=str, required=True, help="Liste de mots pour le paradigme")
     parser.add_argument("--zoom", type=str, choices=['Activé', 'Désactivé'], required=True, help="Activer le Zoom")
-    parser.add_argument("--file",required=False, type=str, help="Liste de mots pour le paradigme")
-
-
-    print("wtf dude")
+    parser.add_argument("--file", type=str, help="Chemin vers le fichier de mots", required=False)
 
     args = parser.parse_args()
-    print(args.file)
-
-
     main(args.duration, args.words, args.zoom, args.file)

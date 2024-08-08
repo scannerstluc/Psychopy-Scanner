19fa4e82c08bfbfbeb002698a3a8f99bd47e6ba7
corentin <corentin.warzee@student.uclouvain.be>
2024-08-08 10:51:31 +0200

    version avec tsv

--- images_psychopy.py
+++ images_psychopy.py
@@ -1,4 +1,6 @@
 import argparse
+import csv
+
 from psychopy import visual, core, event
 import serial
 
@@ -23,7 +25,7 @@ def static_images_psychopy(chemin, duration, betweenstimuli, zoom):
         color=[-0.0118, 0.0039, -0.0196],
         units="pix"
     )
-    chemin="Paradigme_images_statiques/"+chemin
+    chemin = "Paradigme_images_statiques/" + chemin
     images = reading(chemin)
     cross_stim = visual.ShapeStim(
         win=win,
@@ -34,10 +36,13 @@ def static_images_psychopy(chemin, duration, betweenstimuli, zoom):
     )
     thezoom = 1 if zoom else 0.5
     timer = core.Clock()  # Horloge pour le timing précis
-
+    stimulus_times = []  # Liste pour enregistrer les moments des stimuli
+    stimulus_apparition=[]
+    stimuli_liste = []
     cross_stim.draw()
     win.flip()
     wait_for_trigger()
+    global_timer = core.Clock()
 
     for image in images:
         image_path = "Paradigme_images_statiques/stim_static/" + image
@@ -51,23 +56,40 @@ def static_images_psychopy(chemin, duration, betweenstimuli, zoom):
 
         image_stim.draw()
         win.flip()
+        stimulus_apparition.append(global_timer.getTime())
         timer.reset()  # Réinitialiser le timer à chaque nouvelle image
         while timer.getTime() < duration:
             pass  # Attente active jusqu'à la fin de la durée
+        stimulus_times.append(timer.getTime())
+        stimuli_liste.append(image)
 
         # Afficher la croix entre les images
         cross_stim.draw()
         win.flip()
+        stimulus_apparition.append(global_timer.getTime())
         timer.reset()
         while timer.getTime() < betweenstimuli:
             pass  # Attente active entre les images
+        stimulus_times.append(timer.getTime())
+        stimuli_liste.append("Fixation")
+
+
+
 
     win.close()
+    return stimulus_times, stimulus_apparition,stimuli_liste
+
+def write_tsv(onset, duration, trial_type, filename="output.tsv"):
+    with open(filename, mode='w', newline='') as file:
+        tsv_writer = csv.writer(file, delimiter='\t')
+        tsv_writer.writerow(['onset', 'duration', 'trial_type'])
+        for i in range(len(onset)):
+            tsv_writer.writerow([onset[i], duration[i], trial_type[i]])
 
 
 def main(duration, betweenstimuli, file, zoom):
-    #wait_for_trigger()
-    static_images_psychopy(file, duration, betweenstimuli, zoom)
+    stimulus_times, stimulus_apparition, stimuli = static_images_psychopy(file, duration, betweenstimuli, zoom)
+    write_tsv(stimulus_apparition, stimulus_times, stimuli)
 
 
 if __name__ == "__main__":
--- psychopy_runner.py
+++ psychopy_runner.py
@@ -1,67 +1,80 @@
 import argparse
 import serial
 from psychopy import visual, core, event
+import csv
 
+class PsychoPyParadigm:
+    def __init__(self, duration, words, zoom, file):
+        self.duration = int(duration)
+        self.words = words
+        self.zoom = zoom == "Activé"
+        self.file = file
+        self.stimuli_times = []
+        self.stimuli_apparition = []
+        self.stimuli = []
+        self.global_timer= core.Clock()
 
-def wait_for_trigger(port='COM3', baudrate=9600, trigger_char='s'):
-    with serial.Serial(port, baudrate=baudrate) as ser:
-        trigger = ser.read().decode('utf-8')
-        while trigger != trigger_char:
+    def wait_for_trigger(self, port='COM3', baudrate=9600, trigger_char='s'):
+        with serial.Serial(port, baudrate=baudrate) as ser:
             trigger = ser.read().decode('utf-8')
-        print("Trigger received")
+            while trigger != trigger_char:
+                trigger = ser.read().decode('utf-8')
+            print("Trigger received")
 
+    def affichage_mots(self, win, text_stim, words, display_time):
+        timer = core.Clock()
+        for word in words:
+            text_stim.setText(word)
+            text_stim.draw()
+            win.flip()
+            self.stimuli_apparition.append(self.global_timer.getTime())  # Enregistrer le moment où le stimulus apparaît
+            timer.reset()  # Réinitialiser l'horloge à chaque nouveau mot
+            while timer.getTime() < display_time:
+                pass  # Attendre sans bloquer d'autres processus
+            self.stimuli_times.append(timer.getTime())
+            self.stimuli.append(word)
 
-def affichage_mots(win, text_stim, words, display_time):
-    timer = core.Clock()
-    for word in words:
-        text_stim.setText(word)
-        text_stim.draw()
-        win.flip()
-        timer.reset()  # Réinitialiser l'horloge à chaque nouveau mot
-        while timer.getTime() < display_time:
-            pass  # Attendre sans bloquer d'autres processus
+    def reading(self, filename):
+        filename = "Paradigme_mots/" + filename
+        with open(filename, "r") as fichier:
+            ma_liste = [line.strip() for line in fichier]
+        print(ma_liste)
+        return ma_liste
 
+    def pause_for_seconds(self, seconds):
+        timer = core.Clock()
+        timer.reset()
+        while timer.getTime() < seconds:
+            pass
 
-def reading(filename):
-    filename = "Paradigme_mots/" + filename
-    with open(filename, "r") as fichier:
-        ma_liste = [line.strip() for line in fichier]
-    print(ma_liste)
-    return ma_liste
+    def words_psychopy(self):
+        win = visual.Window(fullscr=True, color=[-1, -1, -1], units='pix')
+        text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=150 if self.zoom else 50)
 
+        self.wait_for_trigger()
+        self.global_timer.reset()
+        nothinkinglist = [":; $+", " #^=-", ":?$µ", "###"]
+        self.affichage_mots(win, text_stim, nothinkinglist, self.duration)
+        self.affichage_mots(win, text_stim, self.words, self.duration)
+        self.affichage_mots(win, text_stim, nothinkinglist, self.duration)
+        self.pause_for_seconds(5)  # Pause de 5 secondes avant de fermer
 
-def pause_for_seconds(seconds):
-    timer = core.Clock()
-    timer.reset()
-    while timer.getTime() < seconds:
-        pass
+        win.close()
 
+    def write_tsv(self, filename="output1.tsv"):
+        with open(filename, mode='w', newline='') as file:
+            tsv_writer = csv.writer(file, delimiter='\t')
+            tsv_writer.writerow(['onset', 'duration', 'trial_type'])
+            for i in range(len(self.stimuli_apparition)):
+                tsv_writer.writerow([self.stimuli_apparition[i], self.stimuli_times[i], self.stimuli[i]])
 
-def words_psychopy(words, display_time, boolean):
-    win = visual.Window(fullscr=True, color=[-1, -1, -1], units='pix')
-    text_stim = visual.TextStim(win, text='', color=[1, 1, 1], height=150 if boolean else 50)
-
-    wait_for_trigger()
-    nothinkinglist = [":; $+", " #^=-", ":?$µ", "###"]
-    affichage_mots(win, text_stim, nothinkinglist, display_time)
-    affichage_mots(win, text_stim, words, display_time)
-    affichage_mots(win, text_stim, nothinkinglist, display_time)
-    pause_for_seconds(5)  # Pause de 5 secondes avant de fermer
-
-    win.close()
-    core.quit()
-
-
-def main(duration, words, zoom, file):
-    print(f"Durée: {duration}")
-    if file:
-        words = reading(file)
-    else:
-        words = [word.strip() for word in words.split(',')]
-
-    #wait_for_trigger()
-    words_psychopy(words, int(duration), zoom == "Activé")
-
+    def run(self):
+        if self.file:
+            self.words = self.reading(self.file)
+        else:
+            self.words = [word.strip() for word in self.words.split(',')]
+        self.words_psychopy()
+        self.write_tsv()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Exécuter le paradigme Psychopy")
@@ -71,4 +84,5 @@ if __name__ == "__main__":
     parser.add_argument("--file", type=str, help="Chemin vers le fichier de mots", required=False)
 
     args = parser.parse_args()
-    main(args.duration, args.words, args.zoom, args.file)
+    paradigm = PsychoPyParadigm(args.duration, args.words, args.zoom, args.file)
+    paradigm.run()
